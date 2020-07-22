bf5467b7f1c48e75a6537e3a9409771390ae771d
Dastan <30517553+Shah-ikbal@users.noreply.github.com>
2020-07-21 18:39:58 -0700

    Add files via upload

---
+++ build/precache-manifest.54625a523a87a5146ea7bc00af9b85b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc34bc7eb4824df1aec8289ee41991c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9b537ba399dea246c22",
+    "url": "/static/css/main.af3cc5aa.chunk.css"
+  },
+  {
+    "revision": "a2a32c42310c8e5827da",
+    "url": "/static/js/2.00c4b2d5.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/static/js/2.00c4b2d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b537ba399dea246c22",
+    "url": "/static/js/main.2567fc4d.chunk.js"
+  },
+  {
+    "revision": "e90a02fb6b6a3d49eb98",
+    "url": "/static/js/runtime-main.70aaa4c8.js"
+  }
+]);
