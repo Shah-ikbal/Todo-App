c45d566c7769e3b7493481d3e402daae82b48945
Dastan <30517553+Shah-ikbal@users.noreply.github.com>
2020-08-16 09:29:58 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
    
-        apiKey: "AIzaSyB_xceh1gaVOnevNeb-1m6xKVFrdWsu6ZY",
-        authDomain: "todo-app-cp-f7cab.firebaseapp.com",
+        apiKey: "your_api_key",
+        authDomain: "your_created_authDomain",
         databaseURL: "https://todo-app-cp-f7cab.firebaseio.com",
         projectId: "todo-app-cp-f7cab",
         storageBucket: "todo-app-cp-f7cab.appspot.com",
         messagingSenderId: "184515607929",
-        appId: "1:184515607929:web:6ae98c8178108d19e143a8",
+        appId: "your_app_id",
         measurementId: "G-G4EVD4NX6H"
     
 
